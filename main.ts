3e92ee7b4054dea029e6d0927b76ff37246e8b47
radekjun <52254710+radekjun@users.noreply.github.com>
2022-07-31 19:56:02 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,115 @@
+basic.forever(function () {
+    basic.showNumber(3)
+    basic.pause(200)
+    basic.showNumber(2)
+    basic.pause(200)
+    basic.showNumber(1)
+    basic.pause(200)
+    basic.showString("GO!")
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        # . . # .
+        . . . # .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . # . # .
+        . . . # .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . . # # .
+        . . . # .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . . . # #
+        . . . # .
+        . . . # .
+        `)
+    basic.showString("FINISH!")
+})
